4b8ef3ec8e72c438258d9d1d7dc02eb5664bcabb
gabriellandgraf <gabriellandgrafdecarvalho54@gmail.com>
2024-08-06 16:36:47 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+$usuarios= ["nome:" => "cleitu",
+			"email" => "cleitindugrau@gmail.com",
+			"idade" => "12"
+];
+
+$variavel=1;
+
+$teste=is_array ($variavel);
+echo $teste;*
+
+$usuarios= ["nome:" => "cleitu",
+			"email" => "cleitindugrau@gmail.com",
+			"idade" => "12"
+];
+
+$teste=array_keys($usuarios);
+print_r($teste);
+
+
+$lista_de_frutas=['maçã','uva','ameixa'];
+
+$num_elementos=count($lista_de_frutas);
+echo $num_elementos;
+
+$pao="crocante,macio,recheado";
+
+$array=explode (",",$pao,-1);
+print_r($array);
+
+
+frase=['o', ' pão', ' não', ' está', ' recheado', ' !'];
+
+$resultado=implode("",$frase);
+echo $resultado;
+
+$num=1;
+
+while ($num<12) {
+   echo $num++;
+}
+
+echo 'fim do loop';
+
+$elantra=1;
+
+do {
+    echo 'entrou no do while';
+} while ($elantra<1);
+
+while ($elantra<1) {
+    echo 'entrou no while';
+};
+
+for ($x=1; $x<=10; $x++){
+    echon "$x \n";
+}
+
+
+$itens= array ('sofá', ' mesa', ' guarda-roupa', ' pia', ' fogão', ' geladeira');
+print_r ($itens);
+
+foreach ($itens as $item) {
+    echo $item;
+    if ($item =='fogão') {
+        echo 'Compre um fogão e ganhe uma panela de pressão clock';
+    }
+}*/
+
+
+?>
